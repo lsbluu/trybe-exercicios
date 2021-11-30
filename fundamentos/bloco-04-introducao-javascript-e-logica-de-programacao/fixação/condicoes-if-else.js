9a7d941fd5965b60c0b6e614bc1db0b5f0fb0c7e
lsbluu <lsblu@hotmail.com>
2021-11-30 11:05:19 -0300

    ifelse

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/fixação/condicoes-if-else.js
@@ -0,0 +1,11 @@
+const nota = 0;
+
+
+if (nota >= 80)
+{console.log("Parabéns");} 
+else if (nota >= 60 && nota < 80 )
+{console.log("Você está na lista de espera");} 
+else if (nota < 60)
+    {console.log("Você foi reprovado");} 
+
+
