1e70bd5b32f9769a5c4cba3e1a0aa72c3c3f98c4
lsbluu <lsblu@hotmail.com>
2021-12-05 17:44:07 -0300

    Numero imppar ===True

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio9.js
@@ -0,0 +1,12 @@
+// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+
+
+const n1 = Math.floor(Math.random()* 100) +1;
+const n2 = Math.floor(Math.random()*100)+ 1;
+const n3 = Math.floor(Math.random()*100)+1;
+
+
+if(n1%2===1 || n2%2===1 || n3%2===1){
+    console.log(false);
+    console.log(n1, "-", n2,"-", n3);
+}
