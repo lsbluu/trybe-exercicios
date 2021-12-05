4e2e3a94844d8f5c759b48d86b9c1aa98fce59eb
lsbluu <lsblu@hotmail.com>
2021-12-05 18:09:06 -0300

    lucro do produto

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio10.js
@@ -0,0 +1,13 @@
+const valorDoProduto = 1000;
+const valorDaVenda = 140;
+const imposto = 1.2;
+
+if(valorDoProduto <=0 || valorDaVenda <=00){
+console.log("Erro");
+
+} else{
+const custoDoProduto = valorDoProduto * imposto;
+const lucro = valorDaVenda - custoDoProduto;
+
+console.log(custoDoProduto)
+console.log(lucro)}
