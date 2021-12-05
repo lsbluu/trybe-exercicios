9b7f982fdcc29b56b9293fd4fcac30a051b1632c
lsbluu <lsblu@hotmail.com>
2021-12-05 11:37:15 -0300

    Maior entre tres numeros

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,12 @@
+const a = 14;
+const b = 18;
+const c = 19;
+
+
+if (a > b && a > c){
+    console.log("Maior é ", a);
+} else if(b > a && b > c){
+    console.log("Maior é)", b);
+} else{
+    console.log("Maior é ", c);
+}
