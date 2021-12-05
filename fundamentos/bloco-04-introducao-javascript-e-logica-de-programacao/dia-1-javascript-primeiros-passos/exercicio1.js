d92d4d8971c6ea7dcc3b566f2e354e4804cf78a8
lsbluu <lsblu@hotmail.com>
2021-12-05 11:30:44 -0300

    operaçao aritmetica

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1.js
@@ -0,0 +1,28 @@
+const a = 5;
+const b = 5;
+let f = 3;
+
+switch (f){
+    case 1:{
+        console.log(a+b);
+        break;
+    }
+    case 2:{
+        console.log(a-b);
+        break;
+    }
+    case 3:{
+        console.log(a*b);
+        break;
+    }
+    case 4:{
+        console.log(a/b);
+        break;
+    }
+    case 5:{
+        console.log(a%b);
+        break;
+    }
+        
+
+}
