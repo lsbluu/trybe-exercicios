0c6bb63d94da518685511860284a756d93d40c97
lsbluu <lsblu@hotmail.com>
2021-12-05 11:40:47 -0300

    Retornar positivo/negativo

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio4.js
@@ -0,0 +1,14 @@
+const valor = "negatdivo";
+
+switch (valor){
+    case "positivo":
+        console.log("positive");
+        break;
+    case "negativo":
+        console.log("negative");
+        break;
+    default:
+        console.log("0")
+        break;
+
+}
