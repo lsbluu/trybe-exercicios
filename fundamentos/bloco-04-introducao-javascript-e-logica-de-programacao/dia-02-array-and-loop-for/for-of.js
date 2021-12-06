a7f65d928427b3958091a6b0ab2c4e36b3e2c97b
lsbluu <lsblu@hotmail.com>
2021-12-06 16:33:13 -0300

    for-of

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-02-array-and-loop-for/for-of.js
@@ -0,0 +1,6 @@
+let names = ['Jão', 'Maria', 'Antonio', 'Margarida'];
+
+for (let i of names){
+    console.log(i);
+
+}
---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-02-array-and-loop-for/for.js
@@ -0,0 +1,6 @@
+let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+
+for(let i = 0; i < groceryList.length; i += 1){
+    console.log(groceryList[i]);
+}
