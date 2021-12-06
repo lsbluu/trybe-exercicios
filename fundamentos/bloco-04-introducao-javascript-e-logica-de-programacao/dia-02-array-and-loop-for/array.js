076a2cdcd6032098d37a4e80123a5dfffee54a83
lsbluu <lsblu@hotmail.com>
2021-12-06 15:42:40 -0300

    exercicios fixacao

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-02-array-and-loop-for/array.js
@@ -0,0 +1,17 @@
+let menu = ["Menu", "Serviços", "Portifolio", "Links"];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+
+let menu1 = ["Menu", "Serviços", "Portifolio", "Links"];
+let indexOfPortifolio = menu1.indexOf("Serviços");
+
+console.log(indexOfPortifolio);
+
+
+let menu2 = ["Menu", "Serviços", "Portifolio", "Links"];
+
+menu2.push("Contato")
+
+console.log(menu2);
