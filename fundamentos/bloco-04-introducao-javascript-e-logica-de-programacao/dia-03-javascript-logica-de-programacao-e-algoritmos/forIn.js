2260346d493c871fe8f6ba8657e79fcb729887e0
lsbluu <lsblu@hotmail.com>
2021-12-20 18:44:32 -0300

    funcçoes

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/forIn.js
@@ -0,0 +1,22 @@
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+
+
+  for(value in names){
+      console.log("Olá", names[value]);
+  }
+
+
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for(key in car){
+      console.log(key, car[key]);
+  }
