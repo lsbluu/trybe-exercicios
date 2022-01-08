1e803566ff79e8e0b6764b7dd7e8836025e5667f
lsbluu <lsblu@hotmail.com>
2022-01-08 10:15:37 -0300

    buscando elementos

---
+++ fundamentos/bloco-05-dom-eventos-webStorage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,32 @@
+console.log(document.getElementById("elementoOndeVoceEsta"))
+
+console.log(document.getElementById("elementoOndeVoceEsta").parentNode.style.color = "blue")
+
+console.log(document.getElementById("primeiroFilhoDoFilho").innerHTML = "Primeiro Filho")
+
+console.log(document.getElementById("pai").firstElementChild)
+
+
+
+console.log(document.getElementById("elementoOndeVoceEsta").previousElementSibling)
+
+
+console.log(document.getElementById("elementoOndeVoceEsta").nextSibling)
+
+
+console.log(document.getElementById("elementoOndeVoceEsta").nextElementSibling)
+
+console.log(document.getElementById("pai").lastElementChild.previousElementSibling)
+
+
+
+
+
+/*OK Acesse o elemento elementoOndeVoceEsta .
+OK Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+OK Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+OK Acesse o primeiroFilho a partir de pai .
+OK Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+OK Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+OK Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+OK Agora acesse o terceiroFilho a partir de pai . */
