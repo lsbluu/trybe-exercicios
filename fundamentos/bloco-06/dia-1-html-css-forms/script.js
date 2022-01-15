154f8a024b8894f8590f4621ac9838caca6cbb75
lsbluu <lsblu@hotmail.com>
2022-01-15 19:32:43 -0300

    Exercicio

--- fundamentos/bloco-06/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-06/dia-1-html-css-forms/script.js
@@ -1,17 +1,21 @@
 const UF = document.querySelector('#estados');
 const estados = ['DF','SP','GO'];
 
-for(let i=0; i<estados.length; i+=1){
-   let ufs = estados[i];
-   let options = document.createElement('option')
 
-   options.innerHTML = ufs;
-   options.value = estados[i];
-
-   UF.appendChild(options);
+function ufs() {
+   for(let i=0; i<estados.length; i+=1){
+      let ufs = estados[i];
+      let options = document.createElement('option')
    
-
-   console.log(i);
+      options.innerHTML = ufs;
+      options.value = estados[i];
+   
+      UF.appendChild(options);
+      
+   
+      console.log(i);
+   }
 }
 
+ufs();
 
