7b4e7d56f238c656b5419c3c805b0968b92075af
lsbluu <lsblu@hotmail.com>
2022-01-23 16:56:28 -0300

    adicionando pasta

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/script.js
@@ -0,0 +1,11 @@
+// function soma (n1,n2) {
+// return n1+n2;
+// }
+
+
+// console.log(soma(2,2));
+
+
+const soma = (n1) => n1+5;
+
+console.log(soma(2));
--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,6 +1,7 @@
 const button = document.getElementById('click');
 const count = document.getElementById('count');
 let clickCount = 0;
+
 button.addEventListener('click', function(){
    
     count.innerHTML = clickCount +=1;
