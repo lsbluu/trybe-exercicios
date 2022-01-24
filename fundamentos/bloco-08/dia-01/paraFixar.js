6e58c3a8233cbbe53ef00676c4059590c97fda74
lsbluu <lsblu@hotmail.com>
2022-01-24 15:42:37 -0300

    para fixar hof

---
+++ fundamentos/bloco-08/dia-01/paraFixar.js
@@ -0,0 +1,9 @@
+const wakeUp = () => "Acordando";
+
+const coffe = () => "Bora tomar café";
+
+const sleep = () => "Partiu dormir";
+
+const doingThings = (funca) => funca();
+
+doingThings(wakeUp);
