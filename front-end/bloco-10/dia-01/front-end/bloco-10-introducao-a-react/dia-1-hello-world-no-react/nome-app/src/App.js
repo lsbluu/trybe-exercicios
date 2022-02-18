6ca667f90d7d061a5666387a744f7a29d06b58df
lsbluu <lsblu@hotmail.com>
2022-02-17 21:09:20 -0300

    exercicio 1

---
+++ front-end/bloco-10/dia-01/front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+const task = (value) => {
+  
+  return (
+    value.map((elemento) => <li>{elemento}</li> )
+  );
+}
+
+const array = [' Estudar ',' Ler ',' Cantar ',' Andar ',' Jogar '];
+
+
+function App() {
+  return (
+  <div>{task(array)}</div>
+  );
+}
+
+export default App;
